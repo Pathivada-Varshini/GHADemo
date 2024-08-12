e1942170961e041828c8066230798aabfb9931aa
Pathivada-Varshini <varshinipathivada20@gmail.com>
2024-08-12 11:53:15 +0530

    test commit

--- src/main/java/com/example/springdemo_docker/SpringdemoDockerApplication.java
+++ src/main/java/com/example/springdemo_docker/SpringdemoDockerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringdemoDockerApplication {
+public class 	SpringdemoDockerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdemoDockerApplication.class, args);
